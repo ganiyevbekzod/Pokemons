140e0fe087284729417be3579f72f9d14cf49619
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2022-12-16 03:18:57 +0500

    pokemons

---
+++ 1-dars/js/main.js
@@ -0,0 +1,58 @@
+var elBody = document.querySelector("body");
+var elList = document.querySelector(".js-list");
+var elSelect=document.querySelector(".js-select")
+
+elBody.style.background="linear-gradient(72.67deg, #1A1822 2.78%, #9040E3 173.27%), #6B50FF";
+function wiew(Array,node) {
+  node.innerHTML='';
+  for(pokemonn of Array){
+    var newItem=document.createElement('li');
+    newItem.setAttribute("class","col-sm col-md-6 col-lg-4 title text-light text-center w-25 m-5 border  border-danger");
+    node.appendChild(newItem);
+  
+    var newId=document.createElement('h4');
+    newId.textContent=`${pokemonn.id}`;
+    newId.setAttribute("class","title");
+    newItem.appendChild(newId);
+    
+    var newImg=document.createElement('img');
+    newImg.setAttribute("src", pokemonn.img)
+    newItem.appendChild(newImg);
+    
+    var newText=document.createElement('h4');
+    newText.textContent=`${pokemonn.name}`;
+    newText.setAttribute("class","title text-light");
+    newItem.appendChild(newText);
+    
+    var newWeight=document.createElement('h4');
+    newWeight.textContent=`${pokemonn.weight}`;
+    newWeight.setAttribute("class","title");
+    newItem.appendChild(newWeight);
+    
+    var newCandy=document.createElement('h4');
+    newCandy.textContent=`${pokemonn.candy}`;
+    newCandy.setAttribute("class","title");
+    newItem.appendChild(newCandy);
+  
+    var newHeight=document.createElement('h4');
+    newHeight.textContent=`${pokemonn.height}`;
+    newHeight.setAttribute("class","title");
+    newItem.appendChild(newHeight);
+  }
+}
+wiew(pokemons,elList)
+let newArray=[];
+elSelect.addEventListener("change",function(){
+
+  newArray=[]
+
+  if(elSelect.value !='All'){
+    pokemons.forEach((pok) => {
+      if(pok.type.includes(elSelect.value)){
+        newArray.push(pok);
+      }
+      wiew(newArray,elList)
+    });
+  }else{
+  wiew(pokemons,elList)}
+})
