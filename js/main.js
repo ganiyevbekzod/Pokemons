37fd34a1c3157534ac1c0de33428063cf205e491
ganiyevbekzod <115441938+ganiyevbekzod@users.noreply.github.com>
2022-12-20 18:09:17 +0500

    search

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ var elBody = document.querySelector("body");
 var elList = document.querySelector(".js-list");
 var elSelect=document.querySelector(".js-select")
 var elOption=document.querySelector(".js-option")
-
+let elForm=document.querySelector(".js-form");
+let elInput=document.querySelector(".js-input");
 elBody.style.background="linear-gradient(72.67deg, #1A1822 2.78%, #9040E3 173.27%), #6B50FF";
+
+
+
 function wiew(Array,node) {
   node.innerHTML='';
   for(pokemonn of Array){
     var newItem=document.createElement('li');
     newItem.setAttribute("class","mb-5 col-sm-12 col-md-6 col-lg-4 title text-light text-center border border-danger");
     node.appendChild(newItem);
-  
+    
     var newId=document.createElement('h4');
     newId.textContent=`${pokemonn.id}`;
     newId.setAttribute("class","title");
@@ -34,7 +38,7 @@ function wiew(Array,node) {
     newCandy.textContent=`${pokemonn.candy}`;
     newCandy.setAttribute("class","title");
     newItem.appendChild(newCandy);
-  
+    
     var newHeight=document.createElement('h4');
     newHeight.textContent=`${pokemonn.height}`;
     newHeight.setAttribute("class","title");
@@ -44,9 +48,9 @@ function wiew(Array,node) {
 wiew(pokemons,elList)
 let newArray=[];
 elSelect.addEventListener("change",function(){
-
+  
   newArray=[]
-
+  
   if(elSelect.value !='All'){
     pokemons.forEach((pok) => {
       if(pok.type.includes(elSelect.value)){
@@ -55,20 +59,34 @@ elSelect.addEventListener("change",function(){
       wiew(newArray,elList)
     });
   }else{
-  wiew(pokemons,elList)}
-})
-// let newSet=new Set();
-
-// pokemons.forEach((item) =>{
-//   item.type.forEach((type) =>{
-//     Array.push(type)
-//   })
-// })
-
+    wiew(pokemons,elList)}
+  })
+  // let newSet=new Set();
+  
+  // pokemons.forEach((item) =>{
+    //   item.type.forEach((type) =>{
+      //     Array.push(type)
+      //   })
+      // })
+      
 
 // newSet.forEach((type) =>{
-//   let newOption=document.createElement('option');
-//   newOption.value=type;
-//   newOption.textContent=type;
-//   elSelect.appendChild(newOption);
-// });
+  //   let newOption=document.createElement('option');
+  //   newOption.value=type;
+  //   newOption.textContent=type;
+  //   elSelect.appendChild(newOption);
+  // });
+  let newArr=[];
+  elForm.addEventListener('input', (evt) =>{
+    evt.preventDefault();
+    newArr=[];
+    elList.innerHTML='';
+    let elInputVal=elInput.value.toLocaleLowerCase();
+    pokemons.forEach((el)=>{
+      if(el.name.toLocaleLowerCase().includes(elInputVal)){
+        newArr.push(el);
+        wiew(newArr,elList)
+      }
+    });
+    console.log(newArr);
+  });
